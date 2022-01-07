94e8f889e2ac5f5aba35c878fdeb2ab9d8911631
ColinFF <3910544+cfframe@users.noreply.github.com>
2022-01-07 16:49:03 +0000

    KAN-10 #comment #close Previous comment registered ok in Jira.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # First trial of GitHub integration with Jira
 # Second trial of GitHub integration with Jira
 # Third trial of GitHub integration with Jira, after resetting Repository access within GitHub
+# Forth trial - close issue.
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
